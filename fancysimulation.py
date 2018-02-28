c5305a1d35949ecb70d3447a6062103ca6197dbf
Lloyd Sommerer <lloyd@lloydandlauren.com>
2018-02-28 14:07:19 -0600

    Fancy simulation progress?

---
+++ fancysimulation.py
@@ -0,0 +1,110 @@
+from asciimatics.effects import Print
+from asciimatics.renderers import BarChart, FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError
+
+from simulation import Simulation
+from virtualtable import VirtualTable
+from sommererbots import SommererBotOne, SommererBotTwo
+from random import seed, randint
+import sys
+
+class FancySimulation(Simulation):
+
+    def run(self):
+        numberOfRounds = 1000
+        while self.has_players():
+            for table in self.tables:
+                table.dealer.take_bets()
+                table.dealer.deal()
+                table.dealer.offer_insurance()
+                table.dealer.play_hands()
+                table.dealer.play_own_hand()
+                table.dealer.payout_hands()
+                try:
+                    Screen.wrapper(graphs)
+                    sys.exit(0)
+                except ResizeScreenError:
+                    pass
+
+def fn():
+    return randint(0, 40)
+
+def graphs(screen):
+    scenes = []
+    if screen.width != 132 or screen.height != 24:
+        effects = [
+            Print(screen, FigletText("Resize to 132x24"),
+                  y=screen.height//2-3),
+        ]
+    else:
+        g1 = Print(screen,
+                  BarChart(10, 40, [fn],
+                           char="=",
+                           gradient=[(20, Screen.COLOUR_GREEN),
+                                     (30, Screen.COLOUR_YELLOW),
+                                     (40, Screen.COLOUR_RED)]),
+                  x=13, y=1, transparent=False, speed=2)
+        # g2 = Print(screen,
+        #           BarChart(
+        #               13, 60,
+        #               [wv(1), wv(2), wv(3), wv(4), wv(5), wv(7), wv(8), wv(9)],
+        #               colour=Screen.COLOUR_GREEN,
+        #               axes=BarChart.BOTH,
+        #               scale=2.0),
+        #           x=68, y=1, transparent=False, speed=2)
+        # g3 = Print(screen,
+        #           BarChart(
+        #               7, 60, [lambda: time.time() * 10 % 101],
+        #               gradient=[
+        #                   (33, Screen.COLOUR_RED, Screen.COLOUR_RED),
+        #                   (66, Screen.COLOUR_YELLOW, Screen.COLOUR_YELLOW),
+        #                   (100, Screen.COLOUR_WHITE, Screen.COLOUR_WHITE),
+        #               ] if screen.colours < 256 else [
+        #                   (10, 234, 234), (20, 236, 236), (30, 238, 238),
+        #                   (40, 240, 240), (50, 242, 242), (60, 244, 244),
+        #                   (70, 246, 246), (80, 248, 248), (90, 250, 250),
+        #                   (100, 252, 252)
+        #               ],
+        #               char=">",
+        #               scale=100.0,
+        #               labels=True,
+        #               axes=BarChart.X_AXIS),
+        #           x=68, y=16, transparent=False, speed=2)
+        # g4 = Print(screen,
+        #           BarChart(
+        #               10, 60,
+        #               [wv2(1), wv2(2), wv2(3), wv2(4), wv2(5), wv2(7), wv2(8), wv2(9)],
+        #               colour=[c for c in range(1, 8)],
+        #               bg=[c for c in range(1, 8)],
+        #               scale=2.0,
+        #               axes=BarChart.X_AXIS,
+        #               intervals=0.5,
+        #               labels=True,
+        #               border=False),
+        #           x=3, y=13, transparent=False, speed=2)
+
+        effects = [g1]
+
+    scenes.append(Scene(effects, -1))
+    screen.play(scenes, stop_on_resize=True)
+
+
+def main():
+    seed(1)
+    simulation = Simulation()
+
+    table1 = VirtualTable(simulation)
+    player1 = SommererBotOne()
+    player1.sit(table1)
+
+    table2 = VirtualTable(simulation)
+    player2 = SommererBotTwo()
+    player2.sit(table2)
+
+    simulation.switch_all_shoes()
+    simulation.run()
+    simulation.results()
+
+#main()
