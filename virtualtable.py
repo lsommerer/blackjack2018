5b024021a593e5f0a028645652a72e911f47eb17
Lloyd Sommerer <lloyd@lloydandlauren.com>
2018-03-12 11:25:31 -0500

    Added commas to the output of large dollar amounts (Python makes this too easy).

--- virtualtable.py
+++ virtualtable.py
@@ -11,7 +11,7 @@ class VirtualTable(Table):
         simulation.add_table(self)
         self._simulation = simulation
         self.finishedPlayers = []
-        bankroll = 1000000
+        bankroll = 1_000_000
         delay = 0
         verbose = False
         dealer = Dealer('Dealer', bankroll, delay, verbose)
